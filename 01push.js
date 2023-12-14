75dfd2e95de6252174f0b90e1ee14e833cc1c5d5
ashishdikshit <ashish.a.dikshit@gmail.com>
2023-12-14 22:26:10 +0530

    sasdsd
    sds

---
+++ 01push.js
@@ -0,0 +1,7 @@
+function func() {
+	let arr = ['ashish', 'arvind', 'akshay'];
+	// Pushing the element into the array
+	arr.push('Dixit');
+	console.log(arr);
+}
+func();
---
+++ 02pop.js
@@ -0,0 +1,8 @@
+function func() {
+  let arr = ["ashish", "arvind", "akshay"];
+  arr.pop("akshay");
+  arr.push("Dixit");
+  console.log(arr);
+}
+func()
+////
